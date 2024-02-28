b4140ea51cb40420cd5880d2cfdfd65e0c7aeb8d
alkanaziz <alkanaziz@gmail.com>
2024-02-28 01:18:14 +0100

    added environment

--- src/pages/PageAbout.tsx
+++ src/pages/PageAbout.tsx
@@ -1,5 +1,7 @@
+import * as config from "../config";
+
 export const PageAbout = () => {
 	return (
-		<p>This is the about page.</p>
-	)
-}
+		<p>This is the about page. ENVIRONMENT=[{config.getEnvironment()}]</p>
+	);
+};
